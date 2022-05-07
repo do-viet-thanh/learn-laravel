b8feebd8811173a3814623ef5ea321884ce46176
Đỗ Viết Thành <dovietthanh105@gmail.com>
2022-05-08 00:42:51 +0700

    add-list-user-view

--- resources/views/users/creat.blade.php
+++ resources/views/users/creat.blade.php
@@ -11,6 +11,193 @@
 
 @section('content')
     <div>
-        content
+        <div class="card">
+            <div class="card-body">
+                <div class="form-group row">
+                    <label for="fname" class="col-sm-3 control-label col-form-label">Email</label>
+                    <div class="col-sm-9">
+                        <input type="email" class="form-control" id="fname" placeholder="Your email"/>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="fname" class="col-sm-3 control-label col-form-label">Password</label>
+                    <div class="col-sm-9">
+                        <input type="password" class="form-control" id="fname" placeholder="Your password"/>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="fname" class="col-sm-3 control-label col-form-label">Confirm password</label>
+                    <div class="col-sm-9">
+                        <input type="password" class="form-control" id="fname" placeholder="Confirm your password"/>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="fname" class="col-sm-3 control-label col-form-label">Username</label>
+                    <div class="col-sm-9">
+                        <input type="text" class="form-control" id="fname" placeholder="Username"/>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="fname" class="col-sm-3 control-label col-form-label">Full name</label>
+                    <div class="col-sm-9">
+                        <input type="text" class="form-control" id="fname" placeholder="Your full name"/>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label class="col-md-3">Gender</label>
+                    <div class="col-md-9">
+                        <div class="form-check">
+                            <input
+                                type="radio"
+                                class="form-check-input"
+                                id="customControlValidation1"
+                                name="radio-stacked"
+                                required
+                            />
+                            <label
+                                class="form-check-label mb-0"
+                                for="customControlValidation1"
+                            >Male</label
+                            >
+                        </div>
+                        <div class="form-check">
+                            <input
+                                type="radio"
+                                class="form-check-input"
+                                id="customControlValidation2"
+                                name="radio-stacked"
+                                required
+                            />
+                            <label
+                                class="form-check-label mb-0"
+                                for="customControlValidation2"
+                            >Female</label
+                            >
+                        </div>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="fname" class="col-sm-3 control-label col-form-label">Phone</label>
+                    <div class="col-sm-9">
+                        <input type="text" class="form-control" id="fname" placeholder="Your phone number"/>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="fname" class="col-sm-3 control-label col-form-label">Date of birth</label>
+                    <div class="col-sm-9">
+                        <input type="text" class="form-control mydatepicker" placeholder="mm/dd/yyyy"/>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label class="col-md-3 mt-3">Phone company</label>
+                    <div class="col-md-9">
+                        <select class="select2 form-select shadow-none" style="width: 100%; height: 36px">
+                            <option>Select</option>
+                            <optgroup label="IOS">
+                                <option value="AK">Iphone</option>
+                            </optgroup>
+                            <optgroup label="Android">
+                                <option value="AZ">Samsung</option>
+                                <option value="CO">Oppo</option>
+                                <option value="ID">Nokia</option>
+                                <option value="MT">Huawei</option>
+                                <option value="NE">Xiaomi</option>
+                                <option value="NM">Realme</option>
+                            </optgroup>
+                        </select>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label class="col-md-3 mt-3">Hobbies</label>
+                    <div class="col-md-9">
+                        <select class="select2 form-select shadow-none mt-3" multiple="multiple" style="height: 36px; width: 100%">
+                            <optgroup label="Land">
+                                <option value="AK" selected>Football</option>
+                                <option value="HI">Badminton</option>
+                                <option value="OR">Climbing</option>
+                                <option value="NV">Riding</option>
+                            </optgroup>
+                            <optgroup label="Underwater">
+                                <option value="CA" selected>Swimming</option>
+                                <option value="WA">Fishing</option>
+                            </optgroup>
+                            <optgroup label="Mountain Time Zone">
+                                <option value="AZ">Arizona</option>
+                                <option value="CO">Colorado</option>
+                                <option value="ID">Idaho</option>
+                                <option value="MT">Montana</option>
+                                <option value="NE">Nebraska</option>
+                                <option value="NM">New Mexico</option>
+                                <option value="ND">North Dakota</option>
+                                <option value="UT">Utah</option>
+                                <option value="WY">Wyoming</option>
+                            </optgroup>
+                            <optgroup label="Central Time Zone">
+                                <option value="AL">Alabama</option>
+                                <option value="AR">Arkansas</option>
+                                <option value="IL">Illinois</option>
+                                <option value="IA">Iowa</option>
+                                <option value="KS">Kansas</option>
+                                <option value="KY">Kentucky</option>
+                                <option value="LA">Louisiana</option>
+                                <option value="MN">Minnesota</option>
+                                <option value="MS">Mississippi</option>
+                                <option value="MO">Missouri</option>
+                                <option value="OK">Oklahoma</option>
+                                <option value="SD">South Dakota</option>
+                                <option value="TX">Texas</option>
+                                <option value="TN">Tennessee</option>
+                                <option value="WI">Wisconsin</option>
+                            </optgroup>
+                            <optgroup label="Eastern Time Zone">
+                                <option value="CT">Connecticut</option>
+                                <option value="DE">Delaware</option>
+                                <option value="FL">Florida</option>
+                                <option value="GA">Georgia</option>
+                                <option value="IN">Indiana</option>
+                                <option value="ME">Maine</option>
+                                <option value="MD">Maryland</option>
+                                <option value="MA">Massachusetts</option>
+                                <option value="MI">Michigan</option>
+                                <option value="NH">New Hampshire</option>
+                                <option value="NJ">New Jersey</option>
+                                <option value="NY">New York</option>
+                                <option value="NC">North Carolina</option>
+                                <option value="OH">Ohio</option>
+                                <option value="PA">Pennsylvania</option>
+                                <option value="RI">Rhode Island</option>
+                                <option value="SC">South Carolina</option>
+                                <option value="VT">Vermont</option>
+                                <option value="VA">Virginia</option>
+                                <option value="WV">West Virginia</option>
+                            </optgroup>
+                        </select>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label class="col-md-3">Avatar</label>
+                    <div class="col-md-9">
+                        <div class="custom-file">
+                            <input type="file" class="custom-file-input" id="validatedCustomFile" required/>
+                            <label class="custom-file-label" for="validatedCustomFile">Choose file...</label>
+                            <div class="invalid-feedback">Example invalid custom file feedback</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="cono1" class="col-sm-3 control-label col-form-label">Introduction</label>
+                    <div class="col-sm-9">
+                        <textarea class="form-control"></textarea>
+                    </div>
+                </div>
+            </div>
+            <div class="border-top">
+                <div class="card-body">
+                    <button type="button" class="btn btn-primary">
+                        Creat user
+                    </button>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
--- resources/views/users/home.blade.php
+++ resources/views/users/home.blade.php
@@ -10,7 +10,93 @@
 @endsection
 
 @section('content')
-    <div>
-        content
+    <div class="content-page teacher-page">
+        <div class="page-breadcrumb">
+            <div class="row">
+                <div class="col-12 d-flex no-block align-items-center">
+                    <h4 class="page-title">Người dùng</h4>
+                </div>
+            </div>
+        </div>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-body">
+
+                            <div class="card-body__head card-body__filter">
+                                <h5 class="card-title bold">Bộ lọc</h5>
+                            </div>
+
+                            <!-- From search -->
+                            <form class="mb-3 d-flex" id="form-search">
+                                <div class="form-row d-flex">
+                                    <div class="col-md-2 m-r-10">
+                                        <input type="text" name="id" class="form-control" placeholder="ID" value="">
+                                    </div>
+                                    <div class="col-md-3 m-r-10">
+                                        <input type="text" name="username" class="form-control" placeholder="Tên" value="">
+                                    </div>
+                                    <button type="submit" class="btn btn-secondary btn-sm">Tìm kiếm</button>
+                                </div>
+
+
+                            </form>
+
+                            <div class="card-body__head d-flex">
+                                <h5 class="card-title">Danh sách người dùng</h5>
+                            </div>
+
+                            <div id="zero_config_wrapper" class="dataTables_wrapper dt-bootstrap4">
+                                <table class="table table-striped table-bordered dataTable" role="grid">
+                                    <thead>
+                                    <tr>
+                                        <th scope="col">ID</th>
+                                        <th scope="col">Username</th>
+                                        <th scope="col">Email</th>
+                                        <th scope="col">Họ tên</th>
+                                        <th scope="col">Số điện thoại</th>
+                                        <th scope="col">Ngày sinh</th>
+                                        <th scope="col">Giới tính</th>
+                                        <th scope="col">Địa chỉ</th>
+                                        <th scope="col">Sở thích</th>
+                                        <th scope="col">Giới thiệu ngắn</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td></td>
+                                            <td></td>
+                                            <td></td>
+                                            <td></td>
+                                            <td></td>
+                                            <td></td>
+                                            <td></td>
+                                            <td></td>
+                                            <td></td>
+                                            <td></td>
+                                            <td>
+                                                <div class="comment-footer d-flex">
+                                                    <a>
+                                                        <button type="button" class="btn btn-cyan btn-xs">Sửa</button>
+                                                    </a>
+                                                    <form method="post">
+                                                        <button type="submit"
+                                                                class="btn-danger btn btn-xs modal_confirm_delete rounded">
+                                                            Xóa
+                                                        </button>
+                                                    </form>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                                <!-- Pagination -->
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
